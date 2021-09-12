a8ec63eeb729db66073a99cd4506f87883e4e59f
Jconan <conanjahh@hotmail.com>
2021-09-12 02:13:22 +0200

    cleanup

--- tools/CustomHooks.tsx
+++ tools/CustomHooks.tsx
@@ -64,31 +64,3 @@ export const UserSession = ({
 };
 
 export const useUserSession = () => useContext(UserSessionContext);
-
-export const useUserState2 = () => {
-  const [userInfo, setUser] = useState<UserInfo | undefined>();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    AsyncStorage.getItem("userInfo")
-      .then((value) => {
-        return (value && (JSON.parse(value) as UserInfo)) || undefined;
-      })
-      .then(setUser)
-      .catch((reason) => console.error(reason))
-      .then(() => setIsLoading(false));
-  }, []);
-
-  return {
-    userInfo,
-    isLoading,
-    store: useCallback((userInfo: UserInfo) => {
-      setUser(userInfo);
-      return AsyncStorage.setItem("userInfo", JSON.stringify(userInfo));
-    }, []),
-    logout: useCallback(() => {
-      setUser(undefined);
-      return AsyncStorage.removeItem("userInfo");
-    }, []),
-  };
-};
